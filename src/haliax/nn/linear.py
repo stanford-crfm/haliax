0ab27d5e18acf16d3a3981566a0aac23ad4bcffe
David Hall <dlwh@stanford.edu>
2023-07-29 00:13:08 -0700

    add an option for Linear to be output-major

--- src/haliax/nn/linear.py
+++ src/haliax/nn/linear.py
@@ -1,11 +1,11 @@
 from typing import Optional
 
 import equinox as eqx
-import jax
 
 import haliax as hax
 
 from ..core import NamedArray
+from ..jax_utils import named_call
 from ..types import AxisSpec
 
 
@@ -20,13 +20,22 @@ class Linear(eqx.Module):
     Out: AxisSpec = eqx.static_field()
 
     @staticmethod
-    def init(In: AxisSpec, Out: AxisSpec, *, key, use_bias=True) -> "Linear":
-        joint_spec = hax.concat_axis_specs(In, Out)
+    def init(In: AxisSpec, Out: AxisSpec, *, key, use_bias=True, out_first: bool = False) -> "Linear":
+        """
+
+        :param In: Input axes
+        :param Out: Output axes
+        :param key: rng key for initialization
+        :param use_bias: whether to include bias term
+        :param out_first: whether to put output axes first in the weight matrix. out_first is how PyTorch does it.
+        :return:
+        """
+        joint_spec = hax.concat_axis_specs(Out, In) if out_first else hax.concat_axis_specs(In, Out)
         weight = hax.random.normal(key, joint_spec) * 0.02
         bias = hax.zeros(Out) if use_bias else None
         return Linear(weight, bias, In, Out)
 
-    @jax.named_scope(name="linear")
+    @named_call
     def __call__(self, inputs):
         q = inputs.dot(self.In, self.weight)
         q = hax.auto_sharded(q)
@@ -36,3 +45,10 @@ class Linear(eqx.Module):
             q = hax.auto_sharded(q)
 
         return q
+
+    @property
+    def out_first(self):
+        if isinstance(self.Out, hax.Axis):
+            return self.weight.axes[0] == self.Out
+        else:
+            return self.weight.axes[: len(self.Out)] == self.Out
