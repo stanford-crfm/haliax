6e02d6aa2116b6ca5a8c73049d9e12c0066ec9df
Rohan Mehta <rohanmehta@Rohans-MacBook-Air.local>
2023-09-07 19:11:09 -0400

    addressed review; added option for user to rename new axis, or just resize by default

--- src/haliax/specialized_fns.py
+++ src/haliax/specialized_fns.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import jax
 import jax.numpy as jnp
 
@@ -5,7 +7,7 @@ from .axis import Axis, AxisSelector
 from .core import NamedArray
 
 
-def top_k(arr: NamedArray, axis: AxisSelector, k: int) -> NamedArray:
+def top_k(arr: NamedArray, axis: AxisSelector, k: int, new_axis: Optional[AxisSelector] = None) -> NamedArray:
     pos = arr._lookup_indices(axis)
     if pos is None:
         raise ValueError(f"Axis {axis} not found in {arr}")
@@ -13,7 +15,11 @@ def top_k(arr: NamedArray, axis: AxisSelector, k: int) -> NamedArray:
     values, _ = jax.lax.top_k(new_array, k=k)
     values = jnp.moveaxis(values, -1, pos)  # move axis back to its original position
 
-    axis = arr.resolve_axis(axis)
-    new_axis = Axis(f"top_{k}_of_{axis.name}", size=k)
+    if new_axis is None:
+        axis = arr.resolve_axis(axis)
+        new_axis = axis.resize(k)
+    elif isinstance(new_axis, str):
+        new_axis = Axis(new_axis, k)
+
     updated_axes = arr.axes[:pos] + (new_axis,) + arr.axes[pos + 1 :]
     return NamedArray(values, updated_axes)
