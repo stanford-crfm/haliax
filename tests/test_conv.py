50fb4dded28f110cb840c41d246054a007368c33
David Hall <dlwh@stanford.edu>
2023-10-08 13:17:46 -0700

    cleanup tests a bit

--- tests/test_conv.py
+++ tests/test_conv.py
@@ -34,12 +34,45 @@ def test_conv_basic_equiv_to_eqx():
     assert hax_output.array.shape == eqx_output.shape
     assert jnp.all(hax_output.array == eqx_output)
 
-    # test weird orders
-    input = input.rearrange(("In", "Height", "Width", "Batch"))
-    hax_output = hax_conv(input).rearrange(("Batch", "Out", "Height", "Width"))
+    # test multibatch
+    input = hax.random.normal(
+        jax.random.PRNGKey(1),
+        (hax.Axis("Batch", 2), hax.Axis("Batch2", 3), In, hax.Axis("Height", 5), hax.Axis("Width", 6)),
+    )
+    hax_output = hax_conv(input)
+    eqx_output = eqx.filter_vmap(eqx.filter_vmap(eqx_conv))(input.array)
 
     assert hax_output.array.shape == eqx_output.shape
     assert jnp.allclose(hax_output.array, eqx_output)
+
+
+def test_conv_grouped_equiv_to_eqx():
+    key = jax.random.PRNGKey(0)
+    In = hax.Axis("In", 4)
+    Out = hax.Axis("Out", 6)
+    hax_conv = Conv.init(("Height", "Width"), In, Out, kernel_size=3, groups=2, key=key)
+    eqx_conv = eqx.nn.Conv(2, 4, 6, kernel_size=3, groups=2, key=key)
+
+    assert hax_conv.weight.array.shape == eqx_conv.weight.shape
+    assert hax_conv.bias.array.shape == eqx_conv.bias.shape[0:1]
+    assert jnp.all(hax_conv.weight.array == eqx_conv.weight)
+
+    input = hax.random.normal(jax.random.PRNGKey(1), (In, hax.Axis("Height", 5), hax.Axis("Width", 6)))
+    eqx_output = eqx_conv(input.array)
+    hax_output = hax_conv(input)
+
+    assert hax_output.array.shape == eqx_output.shape
+    assert jnp.all(hax_output.array == eqx_output)
+
+    # test batched
+    input = hax.random.normal(
+        jax.random.PRNGKey(1), (hax.Axis("Batch", 2), In, hax.Axis("Height", 5), hax.Axis("Width", 6))
+    )
+    hax_output = hax_conv(input)
+    eqx_output = eqx.filter_vmap(eqx_conv)(input.array)
+
+    assert hax_output.array.shape == eqx_output.shape
+    assert jnp.all(hax_output.array == eqx_output)
 
     # test multibatch
     input = hax.random.normal(
@@ -51,6 +84,29 @@ def test_conv_basic_equiv_to_eqx():
 
     assert hax_output.array.shape == eqx_output.shape
     assert jnp.allclose(hax_output.array, eqx_output)
+
+
+def test_conv_weird_order():
+    key = jax.random.PRNGKey(0)
+    In = hax.Axis("In", 3)
+    Out = hax.Axis("Out", 4)
+    hax_conv = Conv.init(("Height", "Width"), In, Out, kernel_size=3, key=key)
+    eqx_conv = eqx.nn.Conv(2, 3, 4, kernel_size=3, key=key)
+
+    assert hax_conv.weight.array.shape == eqx_conv.weight.shape
+    assert hax_conv.bias.array.shape == eqx_conv.bias.shape[0:1]
+    assert jnp.all(hax_conv.weight.array == eqx_conv.weight)
+
+    input = hax.random.normal(
+        jax.random.PRNGKey(1), (hax.Axis("Batch", 2), In, hax.Axis("Height", 5), hax.Axis("Width", 6))
+    )
+    hax_output = hax_conv(input)
+
+    # test weird orders
+    input = input.rearrange(("In", "Height", "Width", "Batch"))
+    hax_output2 = hax_conv(input).rearrange(("Batch", "Out", "Height", "Width"))
+
+    assert jnp.allclose(hax_output.array, hax_output2.array)
 
 
 def test_conv_transpose_basic_equiv_to_eqx():
@@ -83,13 +139,6 @@ def test_conv_transpose_basic_equiv_to_eqx():
     assert hax_output.array.shape == eqx_output.shape
     assert jnp.all(hax_output.array == eqx_output)
 
-    # test weird orders
-    input = input.rearrange(("In", "Height", "Width", "Batch"))
-    hax_output = hax_conv(input).rearrange(("Batch", "Out", "Height", "Width"))
-
-    assert hax_output.array.shape == eqx_output.shape
-    assert jnp.allclose(hax_output.array, eqx_output)
-
     # test multibatch
     input = hax.random.normal(
         jax.random.PRNGKey(1),
@@ -100,3 +149,23 @@ def test_conv_transpose_basic_equiv_to_eqx():
 
     assert hax_output.array.shape == eqx_output.shape
     assert jnp.allclose(hax_output.array, eqx_output)
+
+
+def test_weird_orders_conv_transpose():
+    key = jax.random.PRNGKey(0)
+    In = hax.Axis("In", 3)
+    Out = hax.Axis("Out", 4)
+    hax_conv = ConvTranspose.init(
+        ("Height", "Width"), In, Out, kernel_size=3, dilation=2, output_padding=1, stride=2, key=key
+    )
+
+    input = hax.random.normal(
+        jax.random.PRNGKey(1), (hax.Axis("Batch", 2), In, hax.Axis("Height", 5), hax.Axis("Width", 6))
+    )
+    hax_output = hax_conv(input)
+
+    # test weird orders
+    input = input.rearrange(("In", "Height", "Width", "Batch"))
+    hax_output2 = hax_conv(input).rearrange(("Batch", "Out", "Height", "Width"))
+
+    assert jnp.allclose(hax_output.array, hax_output2.array)
