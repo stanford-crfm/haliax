90799b0716b912428f1ca016f9a8410c156ccaa7
Jason Wang <blahblahj.wsy@gmail.com>
2025-02-22 22:29:42 -0800

    formatting

--- tests/test_int8.py
+++ tests/test_int8.py
@@ -1,29 +1,17 @@
-import chex
-import equinox as eqx
-import jax.numpy as jnp
 import jax.random as jrandom
-import jax.tree_util
-import numpy as np
 from chex import assert_trees_all_close
 
 import haliax as hax
-from haliax._src.fp8 import compute_scale
 from haliax.nn import Linear
-from haliax.quantization import (
-    Int8DotGeneralOp,
-    apply_updates,
-    int8_linear_layers,
-    partition_for_grad_overwrite,
-)
+from haliax.quantization import Int8DotGeneralOp
+
 
 def test_int8_is_reasonable():
     In = hax.Axis("In", 8)
     Out = hax.Axis("Out", 8)
     linear = Linear.init(In, Out, key=jrandom.PRNGKey(0), init_scale=0.1)
 
-    int8_linear = Linear.init(
-        In, Out, key=jrandom.PRNGKey(0), dot_general=Int8DotGeneralOp.init(), init_scale=0.1
-    )
+    int8_linear = Linear.init(In, Out, key=jrandom.PRNGKey(0), dot_general=Int8DotGeneralOp.init(), init_scale=0.1)
 
     input = hax.random.normal(jrandom.PRNGKey(3), In)
     output = linear(input)
